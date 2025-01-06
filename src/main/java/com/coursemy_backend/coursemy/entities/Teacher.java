5d25b9ca8ea14528bf1dcb91ee38dde314d9542a
Raul U. M <raul.ugia@hotmail.com>
2025-01-06 17:58:27 +0100

    Teacher entity created

---
+++ src/main/java/com/coursemy_backend/coursemy/entities/Teacher.java
@@ -0,0 +1,67 @@
+package com.coursemy_backend.coursemy.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name="teacher")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Email
+    @Column(name="email")
+    private String email;
+
+    public Teacher(){
+
+    }
+
+    public Teacher(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
