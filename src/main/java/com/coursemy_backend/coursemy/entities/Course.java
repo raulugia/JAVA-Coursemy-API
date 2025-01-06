d1ebc0602be4caee861acd355c0e411059a36d44
Raul U. M <raul.ugia@hotmail.com>
2025-01-06 18:04:04 +0100

    Course entity created

---
+++ src/main/java/com/coursemy_backend/coursemy/entities/Course.java
@@ -0,0 +1,65 @@
+package com.coursemy_backend.coursemy.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="imageUrl")
+    private String imageUrl;
+
+    public Course(){
+
+    }
+
+    public Course(String name, String description, String imageUrl){
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
