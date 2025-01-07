dfcab0b983c6d46219c1a53d68d972f3d2f28e10
Raul U. M <raul.ugia@hotmail.com>
2025-01-07 18:11:27 +0100

    CustomUserService changed to handle student case

--- src/main/java/com/coursemy_backend/coursemy/repository/StudentRepository.java
+++ src/main/java/com/coursemy_backend/coursemy/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.coursemy_backend.coursemy.repository;
 import com.coursemy_backend.coursemy.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+import java.util.Optional;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }
--- src/main/java/com/coursemy_backend/coursemy/security/CustomUserDetailsService.java
+++ src/main/java/com/coursemy_backend/coursemy/security/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.coursemy_backend.coursemy.security;
 
+import com.coursemy_backend.coursemy.entities.Student;
 import com.coursemy_backend.coursemy.entities.Teacher;
+import com.coursemy_backend.coursemy.repository.StudentRepository;
 import com.coursemy_backend.coursemy.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,19 +10,33 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private TeacherRepository teacherRepository;
+    private StudentRepository studentRepository;
 
     @Autowired
-    CustomUserDetailsService(TeacherRepository teacherRepository){
+    CustomUserDetailsService(TeacherRepository teacherRepository, StudentRepository studentRepository){
         this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Teacher teacher = teacherRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        Optional<Teacher> teacher = teacherRepository.findByEmail(email);
+        if(teacher.isPresent()){
+            Teacher dbTeacher = teacher.get();
+            return new CustomUserDetails(dbTeacher.getEmail(), dbTeacher.getPassword());
+        }
 
-        return new CustomUserDetails(teacher.getEmail(), teacher.getPassword());
+        Optional<Student> student = studentRepository.findByEmail(email);
+        if(student.isPresent()){
+            Student dbStudent = student.get();
+            return new CustomUserDetails(dbStudent.getEmail(), dbStudent.getPassword());
+        }
+
+        throw new UsernameNotFoundException("User not found");
     }
 }
