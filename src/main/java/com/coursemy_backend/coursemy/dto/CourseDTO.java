525baf13aa4d7436e6eb35fd5b197817948300b5
Raul U. M <raul.ugia@hotmail.com>
2025-01-08 16:51:08 +0100

    validation annotations added to courseCTO

--- src/main/java/com/coursemy_backend/coursemy/dto/CourseDTO.java
+++ src/main/java/com/coursemy_backend/coursemy/dto/CourseDTO.java
@@ -1,9 +1,26 @@
 package com.coursemy_backend.coursemy.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class CourseDTO {
+    @Size(min = 5, max = 50, message = "Name must be between 5 and 50 characters")
+    @NotEmpty(message = "Course name cannot be empty")
+    @NotNull(message = "Course name is required")
     private String name;
+
+    @Size(min = 50, max = 1000, message = "Description must be between 100 and 1000 characters")
+    @NotEmpty(message = "Description cannot be empty")
+    @NotNull(message = "Description is required")
     private String description;
+
     private String imageUrl;
+
+    @Min(value = 1, message = "Teacher id cannot be smaller than 1")
+    @NotEmpty(message = "Teacher id cannot be empty")
+    @NotNull(message = "Teacher id is required")
     private long id;
 
     public CourseDTO(String name, String description, String imageUrl, long id) {
